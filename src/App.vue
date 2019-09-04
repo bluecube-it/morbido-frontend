60cc39f1ec0a95dac74da25928e4f0f9442706ab
Renato Pozzi <renato.pozzi.dev@gmail.com>
2019-09-04 12:50:30 +0200

    Set up first layout

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,71 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Morbido.ml</a>
+      <input class="form-control form-control-dark w-100" type="text" placeholder="Search"
+        aria-label="Search">
+      <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+          <a class="nav-link" href="#">Sign out</a>
+        </li>
+      </ul>
+    </nav>
+
+    <div class="container-fluid">
+      <div class="row">
+        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+          <div class="sidebar-sticky">
+            <ul class="nav flex-column">
+              <router-link to="/dashboard">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  <activity-icon></activity-icon>
+                  Dashboard <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              </router-link>
+            </ul>
+
+            <h6
+              class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+              <span>Menu Header</span>
+              <a class="d-flex align-items-center text-muted" href="#"
+                aria-label="Add a new report">
+                <span data-feather="plus-circle"></span>
+              </a>
+            </h6>
+            <ul class="nav flex-column mb-2">
+              <router-link to="/about">
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <calendar-icon></calendar-icon>
+                  Current month
+                </a>
+              </li>
+              </router-link> 
+            </ul>
+          </div>
+        </nav>
+
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+          <router-view/>
+        </main>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+  import {
+    ActivityIcon,
+    CalendarIcon
+  } from 'vue-feather-icons';
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  export default {
+    components: {
+      ActivityIcon,
+      CalendarIcon
+    }
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
