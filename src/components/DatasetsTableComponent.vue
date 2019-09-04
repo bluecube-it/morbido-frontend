fbda8a2f7ea24d4538c9144e202f6bfafb222752
Renato Pozzi <renato.pozzi.dev@gmail.com>
2019-09-04 15:43:05 +0200

    Added plugin for upload and table list of uploaded datasets

---
+++ src/components/DatasetsTableComponent.vue
@@ -0,0 +1,68 @@
+<template>
+  <table class="table card-table table-vcenter table-bordered table-hover">
+    <thead>
+      <tr>
+        <th>Filename</th>
+        <th>Columns</th>
+        <th>Rows</th>
+        <th class="text-right">Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="text-nowrap">weather_list.csv</td>
+        <td class="text-nowrap">12</td>
+        <td class="text-nowrap">872</td>
+        <td class="text-right">
+          <trash-icon class="cursor-pointer" />
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  import { TrashIcon } from 'vue-feather-icons';
+
+  export default {
+    beforeMount() {
+      //this.getData();
+    },
+    components: {
+      TrashIcon
+    },
+    data() {
+      return {
+        rows: [],
+        pagination: {
+          pageNumber: 0,
+          pageSize: 10,
+          pageCount: 0
+        },
+      };
+    },
+    props: {
+
+    },
+    methods: {
+      // Pagination
+      nextPage() {
+        this.pagination.pageNumber++;
+      },
+      prevPage() {
+        this.pagination.pageNumber--;
+      },
+      // Filtering
+      deleteFilters() {
+        this.filters = {};
+      }
+    },
+  };
+
+</script>
+
+<style scoped>
+  .cursor-pointer {
+    cursor: pointer;
+  }
+</style>
---
+++ src/components/FileUpload.vue
@@ -0,0 +1,37 @@
+<template>
+    <file-pond
+        name="dataset"
+        ref="pond"
+        label-idle="Drop files here..."
+        v-bind:allow-multiple="true"
+        accepted-file-types="text/csv"
+        server="/upload/dataset"
+        v-on:init="handleFilePondInit"
+    />
+</template>
+
+<script>
+// Import Vue FilePond
+import vueFilePond from 'vue-filepond';
+
+// Import FilePond styles
+import 'filepond/dist/filepond.min.css';
+
+// Import image preview and file type validation plugins
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+
+// Create component
+const FilePond = vueFilePond(FilePondPluginFileValidateType);
+
+export default {
+    name: 'file-upload',
+    methods: {
+        handleFilePondInit: function() {
+            // FilePond instance methods are available on `this.$refs.pond`
+        }
+    },
+    components: {
+        FilePond
+    }
+};
+</script>
