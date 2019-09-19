5f5d4921e5a026064fd7dd07a95e7a39a7618de9
Renato Pozzi <renato.pozzi.dev@gmail.com>
2019-09-19 17:03:45 +0200

    Added column selection in time series forecasting

--- src/views/TimeSeriesForecast.vue
+++ src/views/TimeSeriesForecast.vue
@@ -17,14 +17,15 @@
           <div class="row">
             <div class="col-md-6 mb-6">
               <label for="state">Dataset</label>
-              <select class="custom-select d-block w-100" id="state" required="">
+              <select class="custom-select d-block w-100" @change="getDatasetColumns" v-model="dataset">
                 <option value="">Choose...</option>
-                <option v-for="dataset in datasets" v-bind:key="dataset.id" :value="dataset.id">{{ dataset.original_name }}</option>
+                <option v-for="dataset in datasets" v-bind:key="dataset.id" :value="dataset.unique_name">{{ dataset.original_name }}</option>
               </select>
               <div class="invalid-feedback">
                 Please provide a valid state.
               </div>
             </div>
+            <!-- 
             <div class="col-md-3 mb-3">
               <label for="lastName">Predict From</label>
               <input type="date" class="form-control" id="zip" placeholder="" required="">
@@ -39,14 +40,79 @@
                 This field is required
               </div>
             </div>
+            -->
           </div>
           <hr class="mb-4">
+          <div class="row mb-4">
+            <div class="col-md-6 mb-6">
+              <label for="state">Time value</label>
+              <select class="custom-select d-block w-100" v-model="params.index">
+                <option value="">Choose...</option>
+                <option v-for="column in columns" v-bind:key="column" :value="column">{{ column }}</option>
+              </select>
+              <div class="invalid-feedback">
+                Please provide a valid state.
+              </div>
+            </div>
+            <div class="col-md-6 mb-6">
+              <label for="state">Value to be foreseen</label>
+              <select class="custom-select d-block w-100" v-model="params.input">
+                <option value="">Choose...</option>
+                <option v-for="column in columns" v-bind:key="column" :value="column">{{ column }}</option>
+              </select>
+              <div class="invalid-feedback">
+                Please provide a valid state.
+              </div>
+            </div>
+          </div>
+          <div class="row mb-4">
+            <div class="col-md-4 mb-4">
+              <label for="state">Precision</label>
+              <select class="custom-select d-block w-100" v-model="params.precision">
+                <option value="low">Low</option>
+                <option value="high">High</option>
+              </select>
+              <div class="invalid-feedback">
+                Please provide a valid state.
+              </div>
+            </div>
+            <div class="col-md-4 mb-4">
+              <label for="state">Unit Time</label>
+              <select class="custom-select d-block w-100" v-model="params.seasonality">
+                <option value="yearly">Yearly</option>
+                <option value="montly">Montly</option>
+              </select>
+              <div class="invalid-feedback">
+                Please provide a valid state.
+              </div>
+            </div>
+            <div class="col-md-4 mb-4">
+              <label for="state">How Many?</label>
+              <select class="custom-select d-block w-100" v-model="params.prediction">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+              </select>
+              <div class="invalid-feedback">
+                Please provide a valid state.
+              </div>
+            </div>
+          </div>
           <b-button variant="dark" block @click="makePrediction">
             <b-spinner small v-if="isLoading"></b-spinner>
             <span v-if="!isLoading">Predict Time Series</span>
           </b-button>
         </form>
-        <div class="mt-5">
+        <div class="mt-5" v-if="this.graph.length > 0">
           <line-chart :data="graph" height="500px" :dataset="{borderWidth: 5}"/>
         </div>
       </div>
@@ -57,7 +123,7 @@
 
 <script>
   import axios from 'axios';
-import { endpoint } from '../utils/utils';
+  import { endpoint } from '../utils/utils';
 
   export default {
     name: 'time-series-forecast',
@@ -70,36 +136,19 @@ import { endpoint } from '../utils/utils';
     data() {
       return {
         datasets: [],
+        columns: [],
         isLoading: false,
+        dataset: '',
+        params: {
+          seasonality: 'yearly',
+          precision: 'low',
+          filename: 'dataset_finale.csv',
+          index: '',
+          input: '',
+          prediction: 1
+        },
         graph: [
-          /*
-          { 
-            name: 'Prediction',
-            data: {
-              '2017-01-01': 3,
-              '2017-01-02': 4,
-              '2017-01-03': 20,
-              '2017-01-04': 10,
-              '2017-01-05': 6,
-              '2017-01-06': 18,
-              '2017-01-07': 18,
-              '2017-01-08': 10,
-            }
-          },
-          {
-            name: 'Real Data', 
-            data: {
-              '2017-01-01': 5,
-              '2017-01-02': 4,
-              '2017-01-03': 18,
-              '2017-01-04': 16,
-              '2017-01-05': 8,
-              '2017-01-06': 20,
-              '2017-01-07': 24,
-              '2017-01-08': 12,
-            }
-          }
-          */
+
         ]
       }
     },
@@ -109,11 +158,16 @@ import { endpoint } from '../utils/utils';
           .then(res => this.datasets = res.data)
           .catch(err => console.log(err));
       },
+      getDatasetColumns() {
+        axios.post(endpoint + '/datasets/columns', { dataset: this.dataset })
+          .then(res => this.columns = res.data.columns)
+          .catch(err => console.log(err));
+      },
       makePrediction() {
         this.isLoading = true;
-        axios.post(endpoint + '/forecasts/sarima')
+        axios.post(endpoint + '/forecasts/sarima', this.params)
           .then(response => {
-            console.log(response.data);
+            this.graph = [];
             this.graph.push(response.data);
             this.isLoading = false;
           })
