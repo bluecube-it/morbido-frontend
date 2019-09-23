d322a4adc20fd40d165298a019d9350e54f3de00
Renato Pozzi <renato.pozzi.dev@gmail.com>
2019-09-23 17:57:15 +0200

    Fixes, removed client secret

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -30,8 +30,8 @@ export default {
                 password: '',
                 grant_type: 'password',
                 scope: '*',
-                client_id: '2',
-                client_secret: 'v7t2xvmmwMJkOjWRL3KT9QXqh9PnDwamSiEVf3i9'
+                client_id: '1',
+                // client_secret: 'v7t2xvmmwMJkOjWRL3KT9QXqh9PnDwamSiEVf3i9'
             },
             isError: false,
             isLoading: false,
@@ -40,7 +40,7 @@ export default {
     methods: {
         login() {
             this.isError = false;
-            axios.post(endpoint + '/v1/oauth/token', this.user)
+            axios.post(endpoint + '/auth/token', this.user)
                 .then(res => this.process(res.data))
                 .catch(err => this.isError = true);
         },
