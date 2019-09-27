ff6458e88c699944bf25854334ca55bb7f099a91
Renato Pozzi <renato.pozzi.dev@gmail.com>
2019-09-27 15:48:57 +0200

    Removed searchbar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,8 +2,8 @@
     <div>
         <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
       <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Morbido.ml</a>
-      <input class="form-control form-control-dark w-100" type="text" placeholder="Search"
-        aria-label="Search">
+      <!-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search"
+        aria-label="Search"> -->
       <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
           <a class="nav-link" style="cursor:pointer;" @click="logout">Sign out</a>
