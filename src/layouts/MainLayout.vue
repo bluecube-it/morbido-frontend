67899e1c9880d95b8d9390142ad77dc67bd53d67
Renato Pozzi <renato.pozzi.dev@gmail.com>
2019-09-27 15:07:24 +0200

    Some linting

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -24,7 +24,9 @@
               </li>
             </ul>
 
-            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+            <h6 class="sidebar-heading d-flex
+              justify-content-between align-items-center
+              px-3 mt-4 mb-1 text-muted">
               <span>Data Management</span>
               <a class="d-flex align-items-center text-muted" href="#"
                 aria-label="Add a new report">
--- src/views/TimeSeriesForecast.vue
+++ src/views/TimeSeriesForecast.vue
@@ -10,31 +10,51 @@
                 <h4 class="d-flex justify-content-between align-items-center mb-3">
                     <span class="text-muted">Instructions</span>
                 </h4>
-                <p>To start a forecast you first need to choose one of the previously uploaded datasets.</p>
-                <p>Then, in the &quot;Time value&quot; box you must select an option that represents a date &frasl; time format, while in the &quot;Value to be foreseen&quot; box you can select whichever option you&rsquo;re interested in making the prediction about.</p>
+                <p>To start a forecast you first need to
+                    choose one of the previously uploaded datasets.</p>
+                <p>Then, in the &quot;Time value&quot;
+                    box you must select an option that represents a date &frasl;
+                    time format, while in the &quot;Value to be foreseen&quot;
+                    box you can select whichever option you&rsquo;re interested
+                    in making the prediction about.</p>
                 <p>To date, the predictable time periods are monthly and annual.</p>
                 <p>The predicted value is the mean on selected period.</p>
                 <h6>Suggestions</h6>
                 <div class="link-list-wrapper">
                     <ul class="link-list">
-                        <li>Selecting a high precision for datasets containing more than 100 lines reduces the time required for the forecast.</li>
-                        <li>To avoid divergent predictions it's better not to forecast a period longer than one third of you dataset's time span. For example, if your time span is 10 years, just forecast 3 (or less) years.</li>
+                        <li>Selecting a high precision for datasets containing
+                            more than 100 lines reduces the time required for the forecast.</li>
+                        <li>To avoid divergent predictions it's better not to forecast a
+                            period longer than one third of you dataset's time span.
+                            For example, if your time span is 10 years,
+                            just forecast 3 (or less) years.</li>
                         <li>It is not recommended to predict single months.</li>
                     </ul>
                 </div>
             </div>
             <div class="col-md-8 order-md-1">
                 <h4 class="mb-3">Configuration</h4>
-                <ValidationObserver ref="observer" v-slot="{ invalid }" tag="form" @submit.prevent="makePrediction">
+                <ValidationObserver ref="observer"
+                    v-slot="{ invalid }"
+                    tag="form"
+                    @submit.prevent="makePrediction">
                     <div class="row">
                         <div class="col-md-6 mb-6">
                             <validation-provider rules="required" v-slot="{ validated, invalid }">
                                 <label for="state">Dataset</label>
-                                <select class="custom-select d-block w-100" v-bind:class="{ 'is-invalid': validated && invalid }" @change="getDatasetColumns" v-model="params.filename">
+                                <select class="custom-select d-block w-100"
+                                    v-bind:class="{ 'is-invalid': validated && invalid }"
+                                    @change="getDatasetColumns"
+                                    v-model="params.filename">
                                     <option value>Choose...</option>
-                                    <option v-for="dataset in datasets" v-bind:key="dataset.id" :value="dataset.unique_name">{{ dataset.original_name }}</option>
+                                    <option v-for="dataset in datasets"
+                                        v-bind:key="dataset.id"
+                                        :value="dataset.unique_name">
+                                        {{ dataset.original_name }}
+                                    </option>
                                 </select>
-                                <div v-if="validated && invalid" class="invalid-feedback">Mandatory Field</div>
+                                <div v-if="validated && invalid"
+                                    class="invalid-feedback">Mandatory Field</div>
                             </validation-provider>
                         </div>
                     </div>
@@ -43,21 +63,35 @@
                         <div class="col-md-6 mb-6">
                             <validation-provider rules="required" v-slot="{ validated, invalid }">
                                 <label for="state">Time value</label>
-                                <select class="custom-select d-block w-100" v-bind:class="{ 'is-invalid': validated && invalid }" v-model="params.index">
+                                <select class="custom-select d-block w-100"
+                                    v-bind:class="{ 'is-invalid': validated && invalid }"
+                                    v-model="params.index">
                                     <option value>Choose...</option>
-                                    <option v-for="column in columns" v-bind:key="column" :value="column">{{ column }}</option>
+                                    <option v-for="column in columns"
+                                        v-bind:key="column"
+                                        :value="column">{{ column }}</option>
                                 </select>
-                                <div v-if="validated && invalid" class="invalid-feedback">Mandatory Field</div>
+                                <div v-if="validated && invalid"
+                                    class="invalid-feedback">Mandatory Field</div>
                             </validation-provider>
                         </div>
                         <div class="col-md-6 mb-6">
                             <validation-provider rules="required" v-slot="{ validated, invalid }">
                                 <label for="state">Time value</label>
-                                <select class="custom-select d-block w-100" v-bind:class="{ 'is-invalid': validated && invalid }" v-model="params.input">
+                                <select class="custom-select d-block w-100"
+                                    v-bind:class="{ 'is-invalid': validated && invalid }"
+                                    v-model="params.input">
                                     <option value>Choose...</option>
-                                    <option v-for="column in columns" v-bind:key="column" :value="column">{{ column }}</option>
+                                    <option v-for="column in columns"
+                                        v-bind:key="column"
+                                        :value="column">
+                                            {{ column }}
+                                    </option>
                                 </select>
-                                <div v-if="validated && invalid" class="invalid-feedback">Mandatory Field</div>
+                                <div v-if="validated && invalid"
+                                    class="invalid-feedback">
+                                    Mandatory Field
+                                </div>
                             </validation-provider>
                         </div>
                     </div>
@@ -71,15 +105,21 @@
                         </div>
                         <div class="col-md-4 mb-4">
                             <label for="state">Unit Time</label>
-                            <select class="custom-select d-block w-100" v-model="params.seasonality" @change="setHMOptions">
-                                <option value="yearly" selected>Yearly</option>
-                                <option value="monthly">Monthly</option>
+                            <select class="custom-select d-block w-100"
+                                v-model="params.seasonality"
+                                @change="setHMOptions">
+                                    <option value="yearly" selected>Yearly</option>
+                                    <option value="monthly">Monthly</option>
                             </select>
                         </div>
                         <div class="col-md-4 mb-4">
                             <label for="state">How Many?</label>
                             <select class="custom-select d-block w-100" v-model="params.prediction">
-                                <option v-for="option in how_many_options" v-bind:value="option.id" :key="option.id" v-text="option.text"></option>
+                                <option v-for="option in how_many_options"
+                                    v-bind:value="option.id"
+                                    :key="option.id"
+                                    v-text="option.text">
+                                </option>
                             </select>
                         </div>
                     </div>
@@ -101,93 +141,93 @@ import axios from 'axios';
 import { endpoint } from '../utils/utils';
 
 export default {
-    name: 'time-series-forecast',
-    mounted() {
-        this.getDatasets();
-    },
-    created() {
-        this.setHMOptions(); // init with default values (yearly)
-    },
-    components: {
-        
-    },
-    data() {
-        return {
-            datasets: [],
-            columns: [],
-            how_many_options: [],
-            isLoading: false,
-            params: {
-                seasonality: 'yearly',
-                precision: 'low',
-                filename: '',
-                index: '',
-                input: '',
-                prediction: 1
-            },
-            graph: []
-        };
-    },
-    methods: {
-        setHMOptions(event) {
-            let c = 0;
-            this.how_many_options = []; // reset the select's options
-            if (event) {
-                // on change was triggered
-                if (event.target.value == 'monthly') {
-                    c = 12;
-                } else {
-                    c = 3;
-                }
-            } else {
-                // just init with default values (yearly)
-                c = 3;
-            }
-            // set options
-            // this.how_many_options.push({ text: 'Choose...', id: 'undefined'});
-            for (let i = 1; i <= c; i++) {
-                this.how_many_options.push({ text: `${i}`, id: `${i}` });
-            }
-        },
-        getDatasets() {
-            axios
-                .get(`${endpoint}/datasets`)
-                .then(res => {
-                    this.datasets = res.data;
-                })
-                .catch(err => console.log(err));
-        },
-        getDatasetColumns() {
-            axios
-                .post(`${endpoint}/datasets/columns`, {
-                    dataset: this.params.filename
-                })
-                .then(res => {
-                    this.columns = res.data.columns;
-                })
-                .catch(err => console.log(err));
-        },
-        async makePrediction() {
-            
-            const isValid = await this.$refs.observer.validate();
+  name: 'time-series-forecast',
+  mounted() {
+    this.getDatasets();
+  },
+  created() {
+    this.setHMOptions(); // init with default values (yearly)
+  },
+  components: {
 
-            if (!isValid) {
-                return false;
-            }
-
-            this.isLoading = true;
-            axios
-                .post(`${endpoint}/forecasts/sarima`, this.params)
-                .then(response => {
-                    this.graph = [];
-                    this.graph.push(response.data);
-                    this.isLoading = false;
-                })
-                .catch(err => {
-                    this.isLoading = false;
-                    alert('Something went wrong with the request..')
-                });
+  },
+  data() {
+    return {
+      datasets: [],
+      columns: [],
+      how_many_options: [],
+      isLoading: false,
+      params: {
+        seasonality: 'yearly',
+        precision: 'low',
+        filename: '',
+        index: '',
+        input: '',
+        prediction: 1,
+      },
+      graph: [],
+    };
+  },
+  methods: {
+    setHMOptions(event) {
+      let c = 0;
+      this.how_many_options = []; // reset the select's options
+      if (event) {
+        // on change was triggered
+        if (event.target.value === 'monthly') {
+          c = 12;
+        } else {
+          c = 3;
         }
+      } else {
+        // just init with default values (yearly)
+        c = 3;
+      }
+      // set options
+      // this.how_many_options.push({ text: 'Choose...', id: 'undefined'});
+      for (let i = 1; i <= c; i += 1) {
+        this.how_many_options.push({ text: `${i}`, id: `${i}` });
+      }
     },
+    getDatasets() {
+      axios
+        .get(`${endpoint}/datasets`)
+        .then((res) => {
+          this.datasets = res.data;
+        });
+      // .catch(err => console.log(err));
+    },
+    getDatasetColumns() {
+      axios
+        .post(`${endpoint}/datasets/columns`, {
+          dataset: this.params.filename,
+        })
+        .then((res) => {
+          this.columns = res.data.columns;
+        });
+      // .catch(err => console.log(err));
+    },
+    async makePrediction() {
+      const isValid = await this.$refs.observer.validate();
+
+      if (!isValid) {
+        return false;
+      }
+
+      this.isLoading = true;
+      axios
+        .post(`${endpoint}/forecasts/sarima`, this.params)
+        .then((response) => {
+          this.graph = [];
+          this.graph.push(response.data);
+          this.isLoading = false;
+        })
+        .catch(() => {
+          this.isLoading = false;
+        });
+
+      return true;
+    },
+  },
 };
 </script>
